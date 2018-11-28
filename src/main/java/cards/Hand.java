02b08668cabb9913e2111132615d562ada75da9a
bms0518 <bms0518@gmail.com>
2018-11-28 00:51:32 -0500

    Fix Nuts

--- src/main/java/cards/Hand.java
+++ src/main/java/cards/Hand.java
@@ -2,113 +2,122 @@ package cards;
 
 import com.google.common.base.Preconditions;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-import java.util.Set;
-import java.util.SortedSet;
-import java.util.TreeSet;
+import java.util.*;
 
 public class Hand implements Iterable<Card> {
 
-  public static final int MAX_CARDS_IN_POKER_HAND = 5;
+    public static final int MAX_CARDS_IN_POKER_HAND = 5;
 
-  private final int maxNumberOfCards;
+    private final int maxNumberOfCards;
 
-  private SortedSet<Card> cardSet = new TreeSet<>();
+    private SortedSet<Card> cardSet = new TreeSet<>();
 
-  private Hand(int maxNumberOfCards) {
-    this.maxNumberOfCards = maxNumberOfCards;
-  }
-
-
-  public void add(Card card) {
-    Preconditions.checkArgument(card != null);
-    Preconditions.checkArgument(cardSet.size() < maxNumberOfCards);
-
-    cardSet.add(card);
-  }
-
-  public void remove(Card card) {
-    Objects.requireNonNull(card);
-    if (cardSet.size() > 0) {
-      cardSet.remove(card);
-    }
-  }
-
-  public static Hand ofMax(Card... cards) {
-    Hand hand = new Hand(cards.length);
-    for (Card card : cards) {
-      hand.add(card);
-    }
-    return hand;
-  }
-
-
-  public static Hand ofPoker(List<Card> cards) {
-    Card[] cardArray = new Card[cards.size() - 1];
-    cardArray = cards.toArray(cardArray);
-    return Hand.ofPoker(cardArray);
-  }
-
-
-  public static Hand ofPoker(Card... cards) {
-    Preconditions.checkArgument(cards != null, "cards is null");
-    Preconditions.checkArgument(cards.length > MAX_CARDS_IN_POKER_HAND - 1,
-        "too many cards for poker hand");
-    Hand hand = new Hand(MAX_CARDS_IN_POKER_HAND);
-
-    if (cards.length == hand.maxNumberOfCards) {
-      for (Card card : cards) {
-        hand.add(card);
-      }
+    private Hand(int maxNumberOfCards) {
+        this.maxNumberOfCards = maxNumberOfCards;
     }
 
-    return hand;
-  }
 
-  public Set<Card> getCards() {
-    return Collections.unmodifiableSet(cardSet);
-  }
+    public void add(Card card) {
+        Preconditions.checkArgument(card != null);
+        Preconditions.checkArgument(cardSet.size() < maxNumberOfCards);
 
-  public Map<CardRank, Integer> getRankCount() {
-    Map<CardRank, Integer> rankCount = new HashMap<>();
-
-    for (Card card : cardSet) {
-      Integer count = rankCount.get(card.getCardRank());
-      if (count == null) {
-        rankCount.put(card.getCardRank(), 1);
-      } else {
-        count++;
-        rankCount.put(card.getCardRank(), count);
-      }
+        cardSet.add(card);
     }
 
-    return rankCount;
-  }
+    public void remove(Card card) {
+        Objects.requireNonNull(card);
+        if (cardSet.size() > 0) {
+            cardSet.remove(card);
+        }
+    }
 
-  @Override
-  public String toString() {
-    return cardSet.toString();
-  }
+    public static Hand ofMax(Card... cards) {
+        Hand hand = new Hand(cards.length);
+        for (Card card : cards) {
+            hand.add(card);
+        }
+        return hand;
+    }
 
-  @Override
-  public Iterator<Card> iterator() {
-    return cardSet.iterator();
-  }
 
-  public Hand combine(Hand deckHand) {
-    Set<Card> cards = new HashSet<>();
-    cards.addAll(this.cardSet);
-    cards.addAll(deckHand.cardSet);
+    public static Hand ofPoker(List<Card> cards) {
+        Card[] cardArray = new Card[cards.size() - 1];
+        cardArray = cards.toArray(cardArray);
+        return Hand.ofPoker(cardArray);
+    }
 
-    Card[] cardArray = new Card[cards.size()];
-    cardArray = cards.toArray(cardArray);
 
-    return Hand.ofMax(cardArray);
-  }
+    public static Hand ofPoker(Card... cards) {
+        Preconditions.checkArgument(cards != null, "cards is null");
+        Preconditions.checkArgument(cards.length > MAX_CARDS_IN_POKER_HAND - 1,
+                "too many cards for poker hand");
+        Hand hand = new Hand(MAX_CARDS_IN_POKER_HAND);
+
+        if (cards.length == hand.maxNumberOfCards) {
+            for (Card card : cards) {
+                hand.add(card);
+            }
+        }
+
+        return hand;
+    }
+
+    public List<Card> asList() {
+        return Collections.unmodifiableList(new ArrayList<>(cardSet));
+    }
+
+    public Set<Card> getCards() {
+        return Collections.unmodifiableSet(cardSet);
+    }
+
+    public Map<CardRank, Integer> getRankCount() {
+        Map<CardRank, Integer> rankCount = new HashMap<>();
+
+        for (Card card : cardSet) {
+            Integer count = rankCount.get(card.getCardRank());
+            if (count == null) {
+                rankCount.put(card.getCardRank(), 1);
+            } else {
+                count++;
+                rankCount.put(card.getCardRank(), count);
+            }
+        }
+
+        return rankCount;
+    }
+
+    public int size() {
+        return cardSet.size();
+    }
+
+    @Override
+    public String toString() {
+        return cardSet.toString();
+    }
+
+    @Override
+    public Iterator<Card> iterator() {
+        return cardSet.iterator();
+    }
+
+    public Hand combine(Hand deckHand) {
+        Set<Card> cards = new HashSet<>();
+        cards.addAll(this.cardSet);
+        cards.addAll(deckHand.cardSet);
+
+        Card[] cardArray = new Card[cards.size()];
+        cardArray = cards.toArray(cardArray);
+
+        return Hand.ofMax(cardArray);
+    }
+
+    public Hand combinePoker(Hand deckHand) {
+        Set<Card> cards = new HashSet<>();
+        cards.addAll(this.cardSet);
+        cards.addAll(deckHand.cardSet);
+
+        Card[] cardArray = new Card[cards.size()];
+        cardArray = cards.toArray(cardArray);
+        return Hand.ofPoker(cardArray);
+    }
 }
--- src/main/java/poker/GuessTheNuts.java
+++ src/main/java/poker/GuessTheNuts.java
@@ -5,74 +5,156 @@ import cards.CardRank;
 import cards.Deck;
 import cards.Hand;
 import cards.Suit;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class GuessTheNuts {
 
-  public static void main(String[] args) {
-    Deck d = Deck.standard();
+    public static void main(String[] args) {
+        Deck deck = Deck.standard();
+        deck.shuffle();
 
-    d.shuffle();
+        Hand hand = Hand.ofPoker(deck.pop(Hand.MAX_CARDS_IN_POKER_HAND));
 
-    Hand hand = Hand.ofPoker(d.pop(Hand.MAX_CARDS_IN_POKER_HAND));
-
-    System.err.println(hand);
-    nuts(new ArrayList<>(hand.getCards()), d.getCards());
-  }
-
-  private static void nuts(List<Card> board, List<Card> deck) {
-
-    List<Hand> hands = mergeToPokerHands(getBoardHands(board), getDeckHands(deck));
-
-    System.err.println(hands.size());
-    for (Hand hand : hands) {
-      System.err.println(hand);
+        nuts(hand, deck);
     }
-    //Evaluate all 5 card hands
-    //Sort and rank.
 
-  }
+    private static void nuts(Hand boardHand, Deck deck) {
+        List<Hand> possibleHandsUsing2HoleCards = mergeToPokerHands(getBoardHandsTrips(boardHand), getDeckHandsSize2(deck));
+        List<Hand> possibleHandsUsing1HoleCards = mergeToPokerHands(getBoardHandsQuads(boardHand), getDeckHandsSize1(deck));
 
-  private static List<Hand> mergeToPokerHands(List<Hand> boardHands, List<Hand> deckHands) {
-    List<Hand> combinedHands = new ArrayList<>();
-    for (Hand boardHand : boardHands) {
-      for (Hand deckHand : deckHands) {
-        combinedHands.add(boardHand.combine(deckHand));
-      }
-    }
-    return combinedHands;
-  }
+        PokerHands currentBestRank = null;
+        Hand currentBestHand = null;
 
 
-  private static List<Hand> getDeckHands(List<Card> deck) {
-    //find all cards to use.
-    List<Hand> deckHands = new ArrayList<>();
-    for (int i = 0; i < deck.size(); i++) {
-      for (int j = i + 1; j < deck.size(); j++) {
-        Card handCard1 = deck.get(i);
-        Card handCard2 = deck.get(j);
-        deckHands.add(Hand.ofMax(handCard1, handCard2));
-      }
-    }
-    return deckHands;
-  }
+        for (Hand hand : possibleHandsUsing2HoleCards) {
+            PokerHands pokerHand = PokerHands.evaluate(hand);
+            if (currentBestRank == null) {
+                currentBestRank = pokerHand;
+            }
 
-  private static List<Hand> getBoardHands(List<Card> board) {
-    //find all trips in hand.
-    List<Hand> boardHands = new ArrayList<>();
-    for (int i = 0; i < board.size(); i++) {
-      for (int j = i + 1; j < board.size(); j++) {
-        for (int k = j + 1; k < board.size(); k++) {
-          Card boardCard1 = board.get(i);
-          Card boardCard2 = board.get(j);
-          Card boardCard3 = board.get(k);
-          boardHands.add(Hand.ofMax(boardCard1, boardCard2, boardCard3));
+            if (currentBestHand == null) {
+                currentBestHand = hand;
+            }
+
+            if (currentBestRank.compareTo(pokerHand) < 1) {
+                currentBestRank = pokerHand;
+                currentBestHand = hand;
+
+                if(currentBestRank.equals(PokerHands.FOUR_OF_A_KIND)){
+                    System.err.println(2);
+                    System.err.println(currentBestHand);
+                }
+            }
+
         }
-      }
-    }
-    return boardHands;
-  }
 
+        for (Hand hand : possibleHandsUsing1HoleCards) {
+            PokerHands pokerHand = PokerHands.evaluate(hand);
+            if (currentBestRank.compareTo(pokerHand) < 1) {
+                currentBestRank = pokerHand;
+                currentBestHand = hand;
+
+                if(currentBestRank.equals(PokerHands.FOUR_OF_A_KIND)){
+                    System.err.println(2);
+                    System.err.println(currentBestHand);
+                }
+
+            }
+
+
+        }
+
+        System.err.println(boardHand);
+        System.err.println(currentBestRank);
+        System.err.println(currentBestHand);
+
+
+        //Evaluate all 5 card hands
+        //Sort and rank.
+
+    }
+
+    private static List<Hand> mergeToPokerHands(List<Hand> boardHands, List<Hand> deckHands) {
+        List<Hand> combinedHands = new ArrayList<>();
+        for (Hand boardHand : boardHands) {
+            for (Hand deckHand : deckHands) {
+                combinedHands.add(boardHand.combine(deckHand));
+            }
+        }
+        return combinedHands;
+    }
+
+
+    private static List<Hand> getDeckHandsSize2(Deck deck) {
+        //find all cards to use.
+        List<Hand> deckHands = new ArrayList<>();
+
+        List<Card> deckCards = deck.getCards();
+        for (int i = 0; i < deckCards.size(); i++) {
+            for (int j = i + 1; j < deckCards.size(); j++) {
+                Card handCard1 = deckCards.get(i);
+                Card handCard2 = deckCards.get(j);
+                deckHands.add(Hand.ofMax(handCard1, handCard2));
+            }
+        }
+        return deckHands;
+    }
+
+    private static List<Hand> getDeckHandsSize1(Deck deck) {
+        //find all cards to use.
+        List<Hand> deckHands = new ArrayList<>();
+
+        List<Card> deckCards = deck.getCards();
+        for (Card card : deckCards) {
+            deckHands.add(Hand.ofMax(card));
+        }
+
+        return deckHands;
+    }
+
+
+    private static List<Hand> getBoardHandsTrips(Hand board) {
+        //find all trips in hand.
+        List<Hand> boardHands = new ArrayList<>();
+
+        List<Card> boardCards = board.asList();
+
+        for (int i = 0; i < boardCards.size(); i++) {
+            for (int j = i + 1; j < boardCards.size(); j++) {
+                for (int k = j + 1; k < boardCards.size(); k++) {
+                    Card boardCard1 = boardCards.get(i);
+                    Card boardCard2 = boardCards.get(j);
+                    Card boardCard3 = boardCards.get(k);
+                    boardHands.add(Hand.ofMax(boardCard1, boardCard2, boardCard3));
+                }
+            }
+        }
+        return boardHands;
+    }
+
+    private static List<Hand> getBoardHandsQuads(Hand board) {
+        //find all trips in hand.
+        List<Hand> boardHands = new ArrayList<>();
+
+        List<Card> boardCards = board.asList();
+
+        for (int i = 0; i < boardCards.size(); i++) {
+            for (int j = i + 1; j < boardCards.size(); j++) {
+                for (int k = j + 1; k < boardCards.size(); k++) {
+                    for (int l = k + 1; l < boardCards.size(); l++) {
+                        Card boardCard1 = boardCards.get(i);
+                        Card boardCard2 = boardCards.get(j);
+                        Card boardCard3 = boardCards.get(k);
+                        Card boardCard4 = boardCards.get(l);
+                        boardHands.add(Hand.ofMax(boardCard1, boardCard2, boardCard3, boardCard4));
+                    }
+                }
+            }
+
+        }
+        return boardHands;
+    }
 
 }
